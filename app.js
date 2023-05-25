9813a995bf51217c4bd1658a19a2dc03b2d7e119
sooto selim <adewaleselim6@gmail.com>
2023-05-25 16:44:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let container=document.querySelector(".container");
+let color1=document.getElementById(".color_1");
+let color2=document.getElementById(".color_2");
+
+
+/* color2.addEventListener('click',() => {
+
+    container.classList.add('change')
+}) */
+function app(){
+    container.classList.add('change')
+}
+function remo(){
+    container.classList.remove('change')
+}
